23dc521dbc39e22465c0766d82e6fa0d75b8af5d
Dan Davydov <dan.davydov1@gmail.com>
2022-09-21 13:55:11 +0300

    Added more security measures

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import orderRoutes from './routes/orders.js'
 import swaggerRoutes from './routes/swagger.js'
+import helmet from 'helmet'
 
 const app = express();
 
 app.use(bodyParser.json({ limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended: true}));
+
+//Remove X-Powered-By Header
+app.use(helmet());
 
 //Allow cross origin resource sharing.
 app.use(cors());
